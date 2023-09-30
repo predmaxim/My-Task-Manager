49cf394fd38d392b6d7daccc37622559808dddb9
Maxim Predybailo <predmaxim@ya.ru>
2023-10-01 00:52:28 +0500

    feat: add ProjectPage

---
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -0,0 +1,15 @@
+import { MainHeader } from 'components/MainHeader';
+import './ProjectsPage.scss';
+
+export default function ProjectsPage() {
+  return (
+    <>
+      <MainHeader />
+      <main className="ProjectsPage">
+        <div className="container">
+          <h1>Projects Page</h1>
+        </div>
+      </main>
+    </>
+  );
+}
