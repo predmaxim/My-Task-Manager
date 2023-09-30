40b29b3b6ce9f6500d2828ba53855def50a8720a
Maxim Predybailo <predmaxim@ya.ru>
2023-10-01 00:52:46 +0500

    feat: add NotFoundPage

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import './NotFoundPage.scss';
+
+export default function NotFoundPage() {
+  return (
+    <main className="notFoundPage">
+      <div className="container">
+        <h1>404 Page not found</h1>
+      </div>
+    </main>
+  );
+}
