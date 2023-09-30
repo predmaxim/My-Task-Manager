e6a7cd18cb53b97bc78e342327d4f60ccbce723c
Maxim Predybailo <predmaxim@ya.ru>
2023-10-01 00:52:13 +0500

    feat: add TasksPage

---
+++ src/pages/TasksPage/TasksPage.tsx
@@ -0,0 +1,11 @@
+import './TasksPage.scss';
+
+export default function TasksPage() {
+  return (
+    <main className="TasksPage">
+      <div className="container">
+        <h1>Tasks Page</h1>
+      </div>
+    </main>
+  )
+}
