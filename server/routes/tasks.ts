9d2db2b881a61e2885d55e102fd4b384eb72ce60
Maxim Predybailo <predmaxim@ya.ru>
2023-10-03 21:33:39 +0500

    feat(server): update routes
    - move URLs from app(index) to routes
    - add full CRUD routes

--- server/routes/tasks.ts
+++ server/routes/tasks.ts
@@ -1,11 +1,33 @@
 import { Router } from 'express';
-import { checkAuth } from '../utils/checkAuth';
-import { getTasks } from '../controllers/tasks';
+import { authCheck } from '../utils/authCheck';
+import {
+  getTasks,
+  getTask,
+  createTask,
+  deleteTask,
+  updateTask
+} from '../controllers/tasks';
 
 const router = Router();
 
 // get Tasks
+// http://localhost:5000/api/tasks
+router.get('/api/tasks/', getTasks);
+
+// Get Task
 // http://localhost:5000/api/tasks/:id
-router.get('/', checkAuth, getTasks);
+router.get('/api/tasks/:id', getTask);
+
+// Create Task
+// http://localhost:5000/api/tasks
+router.post('/api/tasks/', createTask);
+
+// Update Task
+// http://localhost:5000/api/tasks/:id
+router.patch('/api/tasks/:id', updateTask);
+
+// Delete Task
+// http://localhost:5000/api/tasks/:id
+router.delete('/api/tasks/:id', deleteTask);
 
 export default router;
