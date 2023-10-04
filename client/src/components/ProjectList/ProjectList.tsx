e8fb6ac8f2327c05b07559c831b60a038b1d3c6b
Maxim Predybailo <predmaxim@ya.ru>
2023-10-04 13:30:56 +0500

    feat(client): add logic and styles for ProjectList

--- client/src/components/ProjectList/ProjectList.tsx
+++ client/src/components/ProjectList/ProjectList.tsx
@@ -1,14 +1,50 @@
-import { getProjects } from 'backend/controllers/projectController';
+import { useCallback, useEffect, useState } from 'react';
+import axios from 'axios';
+import { nanoid } from 'nanoid';
+import { ProjectType } from 'src/utils/types';
+import { Loading } from '../Loading';
 import './ProjectList.scss';
+import { ButtonWithIcon } from '../ButtonWithIcon';
 
-export async function ProjectList() {
-  const projects = await getProjects();
+export function ProjectList() {
+  const [projects, setProjects] = useState<ProjectType[]>([])
+  const [isLoading, setIsLoading] = useState(true);
+
+
+  const getProjects = useCallback(async () => {
+    try {
+      const URL = `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}/api/projects`;
+      const { data }: { data: { projects: ProjectType[] } } = await axios.get(URL);
+
+      setProjects(data.projects);
+      setIsLoading(false);
+
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    getProjects();
+  }, [getProjects]);
 
   return (
     <div className="ProjectList">
-      {projects.map((el) => {
-        return <button onClick={() => console.log(el.name)}></button>
-      })}
+      {
+        isLoading
+          ? <Loading />
+          : projects.map((project) => {
+            return (
+              <ButtonWithIcon
+                key={nanoid()}
+                className='ProjectButton'
+                icon={project.icon}
+                text={project.name}
+                onClick={() => console.log(project.name)}
+              />
+            )
+          })
+      }
     </div>
   );
 }
