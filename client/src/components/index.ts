4729abb5b864e585864b83f9818ab5642b4bbe59
Maxim Predybailo <predmaxim@ya.ru>
2023-10-04 13:35:24 +0500

    feat(client): add imports to component index

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,4 +1,8 @@
 export { Header } from './Header';
+export { MainHeader } from './MainHeader';
 export { Footer } from './Footer';
 export { ProjectList } from './ProjectList';
 export { Loading } from './Loading';
+export { NewProjectBtn } from './NewProjectBtn';
+export { ButtonWithIcon } from './ButtonWithIcon';
+export { Modal } from './Modal';
