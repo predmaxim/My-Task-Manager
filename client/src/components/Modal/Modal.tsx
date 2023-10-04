4fe0cb59fd249d519ad97729f9f00466396ecdce
Maxim Predybailo <predmaxim@ya.ru>
2023-10-04 23:19:18 +0500

    feat(client): add logic and styles to Modal

--- client/src/components/Modal/Modal.tsx
+++ client/src/components/Modal/Modal.tsx
@@ -2,22 +2,48 @@ import { RiCloseLine } from 'react-icons/ri';
 import './Modal.scss';
 
 export type ModalProps = {
+  className: string,
   isActive: boolean,
   children: JSX.Element,
-  onClose: () => void
+  onClose: () => void,
+  onOk: () => void,
+  header: string,
+  formId?: string,
+  showActionBtns?: boolean
 }
 
-export function Modal({ isActive, children, onClose }: ModalProps) {
+export function Modal({ className, isActive, children, onOk, onClose, header, formId, showActionBtns = true }: ModalProps) {
   const activeClass = isActive ? 'active' : '';
 
   return (
-    <div className="Modal">
-      <div className="Modal__overlay" onClick={onClose} />
+    <div className={`Modal ${className}`}>
+      <div className={`Modal__overlay ${activeClass}`} onClick={onClose} />
       <div className={`Modal__body  ${activeClass}`}>
+
         <RiCloseLine onClick={onClose} className="Modal__closeBtn" />
-        <div className="Modal__content">
-          {children}
-        </div>
+
+        <h5 className="Modal__header">{header}</h5>
+        <div className="Modal__content">{children}</div>
+
+        {
+          showActionBtns &&
+          <div className="actions">
+            <button
+              className="Modal__okBtn"
+              onClick={onOk}
+              type="submit"
+              form={formId}
+            >Ok
+            </button>
+            <button
+              className="Modal__cancelBtn"
+              onClick={onClose}
+              type="button"
+            >Cancel
+            </button>
+          </div>
+        }
+
       </div>
     </div>
   );
