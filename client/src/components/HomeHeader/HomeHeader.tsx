8677a6c367d0a070c6f63880642fa55e2b093547
Maxim Predybailo <predmaxim@ya.ru>
2023-10-06 01:27:47 +0500

    chore(client): rename some components

---
+++ client/src/components/HomeHeader/HomeHeader.tsx
@@ -0,0 +1,20 @@
+import './HomeHeader.scss';
+import logoVertical from 'src/assets/img/logoVertical.svg';
+
+export function HomeHeader() {
+  return (
+    <header className="HomeHeader">
+      <div className="container">
+        <a href="/">
+          <img
+            src={logoVertical}
+            className="logoVertical"
+            alt="logoVertical"
+            width={239}
+            height={169}
+          />
+        </a>
+      </div>
+    </header >
+  );
+}
---
+++ client/src/components/HomeHeader/tests/MainHeader.test.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+import { HomeHeader } from '../HomeHeader';
+
+describe('<HomeHeader />', () => { });
