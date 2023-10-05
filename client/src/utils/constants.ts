6f6cbfce0895b1980bfb22638b720cf9bc19c9ac
Maxim Predybailo <predmaxim@ya.ru>
2023-10-06 01:31:31 +0500

    feat(client): add some constants

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,1 +1,23 @@
 export const BASE_URL = `${process.env.REACT_APP_SERVER_URL}:${process.env.REACT_APP_SERVER_PORT}`;
+
+export const TASK_STATUSES = {
+  queue: 'queue',
+  development: 'development',
+  done: 'done'
+};
+
+export const TASK_PRIORITY = {
+  low: 'low',
+  normal: 'normal',
+  high: 'high'
+};
+
+export const PROJECT_STATUSES = {
+  active: 'active',
+  inactive: 'inactive'
+};
+
+export const ROLES = {
+  admin: 'admin',
+  user: 'user'
+};
