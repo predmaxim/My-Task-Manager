b51526d4cca515663635492820cdef6bf46cf388
Maxim Predybailo <predmaxim@ya.ru>
2023-10-06 20:11:26 +0500

    feat(client): change pages based on new router

--- client/src/pages/NotFoundPage/NotFoundPage.tsx
+++ client/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,19 +1,16 @@
 import { RiArrowLeftLine } from 'react-icons/ri'
-import { Header } from 'src/components';
+import { Link } from 'react-router-dom';
 import './NotFoundPage.scss';
 
 export default function NotFoundPage() {
   return (
-    <>
-      <Header />
-      <main className="NotFoundPage">
-        <div className="container">
-          <h1 className="NotFoundPage__header">404 Page not found</h1>
-          <div className="NotFoundPage__link">
-            <RiArrowLeftLine /><a href="/">Go to Home</a>
-          </div>
+    <main className="NotFoundPage">
+      <div className="container">
+        <h1 className="NotFoundPage__header">404 Page not found</h1>
+        <div className="NotFoundPage__link">
+          <RiArrowLeftLine /><Link to="/">Go to Home</Link>
         </div>
-      </main>
-    </>
+      </div>
+    </main>
   );
-}
+};
--- client/src/pages/TasksPage/TasksPage.tsx
+++ client/src/pages/TasksPage/TasksPage.tsx
@@ -1,15 +1,12 @@
-import { Board, Header } from 'src/components';
+import { Board } from 'src/components';
 import './TasksPage.scss';
 
 export default function TasksPage() {
   return (
-    <>
-      <Header />
-      <main className="TasksPage">
-        <div className="container">
-          <Board />
-        </div>
-      </main>
-    </>
-  )
-}
+    <main className="TasksPage">
+      <div className="container">
+        <Board />
+      </div>
+    </main>
+  );
+};
