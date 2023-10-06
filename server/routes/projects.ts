d1e9d3a7b2366e62eb63a57e4a7258d375f39f80
Maxim Predybailo <predmaxim@ya.ru>
2023-10-07 01:52:23 +0500

    feat(server): change dynamic routes

--- server/routes/projects.ts
+++ server/routes/projects.ts
@@ -16,7 +16,7 @@ router.get('/api/projects/', getProjects);
 
 // Get Project
 // http://localhost:5000/api/projects/:id
-router.get('/api/projects/:id', getProject);
+router.get('/api/projects/:name', getProject);
 
 // Create Project
 // http://localhost:5000/api/projects
@@ -24,10 +24,10 @@ router.post('/api/projects/', createProject);
 
 // Update Project
 // http://localhost:5000/api/projects/:id
-router.patch('/api/projects/:id', updateProject);
+router.patch('/api/projects/:name', updateProject);
 
 // Delete Project
 // http://localhost:5000/api/projects/:id
-router.delete('/api/projects/:id', deleteProject);
+router.delete('/api/projects/:name', deleteProject);
 
 export default router;
