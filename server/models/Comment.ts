fe2c515087f9b833e8433f35cb2d1c2737416104
Maxim Predybailo <predmaxim@ya.ru>
2023-10-07 03:07:20 +0500

    feat(server): change models based on new types

--- server/models/Comment.ts
+++ server/models/Comment.ts
@@ -1,15 +1,14 @@
 import mongoose from 'mongoose';
-import { CommentType } from '../types';
+import { CommentType } from '../utils/types';
 
-const CommentSchema = new mongoose.Schema(
+export const CommentSchema = new mongoose.Schema(
   {
     comment: { type: String, required: true },
     author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     task: { type: mongoose.Schema.Types.ObjectId, ref: 'Task' },
     date: { type: Date, default: Date.now },
-    parent: Boolean || { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }
-  },
-  { timestamps: true },
+    parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }
+  }
 )
 
 export default mongoose.models.Comment || mongoose.model<CommentType>('Comment', CommentSchema);
--- server/models/Task.ts
+++ server/models/Task.ts
@@ -1,25 +1,35 @@
 import mongoose from 'mongoose';
-import { TaskType } from '../types';
+import { TaskType } from '../utils/types';
+import { CommentSchema } from './Comment';
 
-const TaskSchema = new mongoose.Schema<TaskType>({
-  isSubtask: Number || {
-    order: Number
+export const TaskSchema = new mongoose.Schema<TaskType>({
+  number: Number,
+  name: {
+    type: String,
+    minLength: [1, 'Your project name must be longer than 0 characters'],
+    maxLength: [50, 'Your project name must be shorter than 50 characters'],
   },
-  name: String,
-  description: String,
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  project: { type: mongoose.Schema.Types.String, ref: 'Project' },
   created: Date,
+  description: {
+    type: String,
+    minLength: [1, 'Your description must be longer than 0 characters'],
+    maxLength: [1000, 'Your description must be shorter than 1000 characters'],
+  },
   done: Date,
   priority: String,
   status: String,
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  parentTask: { type: mongoose.Schema.Types.ObjectId, ref: 'Task' },
-  project: { type: mongoose.Schema.Types.ObjectId, ref: 'Project' },
+  lastStatus: String,
+  parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Task' },
   due: Date,
   inWork: Date,
-  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }],
   files: [String],
-},
-  { timestamps: true }
+  comments: {
+    type: [CommentSchema],
+    default: {}
+  },
+}
 );
 
 export default mongoose.models.Task || mongoose.model<TaskType>('Task', TaskSchema);
