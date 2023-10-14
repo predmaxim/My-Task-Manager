e2d2e1b425bf1a0c6d137afed8e40603c0963baa
Maxim Predybailo <predmaxim@ya.ru>
2023-10-15 00:07:10 +0500

    style(server): code lint

--- server/routes/projects.ts
+++ server/routes/projects.ts
@@ -1,12 +1,5 @@
 import { Router } from 'express';
-import { authCheck } from '../utils/authCheck';
-import {
-  getProject,
-  getProjects,
-  createProject,
-  deleteProject,
-  updateProject
-} from '../controllers/projects';
+import { createProject, deleteProject, getProject, getProjects, updateProject } from '../controllers/projects';
 
 const router = Router();
 
@@ -15,7 +8,7 @@ const router = Router();
 router.get('/api/projects/', getProjects);
 
 // Get Project
-// http://localhost:5000/api/projects/:id
+// http://localhost:5000/api/projects/:name
 router.get('/api/projects/:name', getProject);
 
 // Create Project
@@ -23,11 +16,11 @@ router.get('/api/projects/:name', getProject);
 router.post('/api/projects/', createProject);
 
 // Update Project
-// http://localhost:5000/api/projects/:id
+// http://localhost:5000/api/projects/:name
 router.patch('/api/projects/:name', updateProject);
 
 // Delete Project
-// http://localhost:5000/api/projects/:id
+// http://localhost:5000/api/projects/:name
 router.delete('/api/projects/:name', deleteProject);
 
 export default router;
