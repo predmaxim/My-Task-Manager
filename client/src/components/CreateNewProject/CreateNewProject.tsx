45a8a0bc4a498092f13f81d0d01bcaf1c6e22b32
Maxim Predybailo <predmaxim@ya.ru>
2023-10-15 00:25:01 +0500

    feat(client): rename NewProject to CreateNewProject, and add logics and styles

---
+++ client/src/components/CreateNewProject/CreateNewProject.tsx
@@ -0,0 +1,133 @@
+import { ButtonWithIcon } from 'components/ButtonWithIcon';
+import { icons } from 'components/icons';
+import { Modal } from 'components/Modal';
+import { ChangeEvent, useCallback, useEffect, useMemo, useState } from 'react';
+import { createPortal } from 'react-dom';
+import { IconType } from 'react-icons';
+import { useDispatch } from 'react-redux';
+import { toast } from 'react-toastify';
+import { createNewProjectThunk } from 'store/asyncActions/createNewProjectThunk';
+import { onActionModal } from 'utils/helpers';
+import { ThunkDispatchType } from 'utils/types';
+import { setCurrentProjectThunk } from '../../store/asyncActions/setCurrentProjectThunk';
+import './CreateNewProject.scss';
+
+export function CreateNewProject() {
+  const dispatch: ThunkDispatchType = useDispatch();
+  const [showNewProjectModal, setNewProjectModal] = useState(false);
+  const [showIconModal, setShowIconModal] = useState(false);
+  const [ProjectIcon, setProjectIcon] = useState<JSX.Element>();
+  const [inputValue, setInputValue] = useState('');
+  const [allIcons, setAllIcons] = useState<JSX.Element[]>();
+
+  const onChangeInputHandler = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
+  const onOkModal = () => {
+    if (inputValue) {
+      dispatch(createNewProjectThunk(inputValue.trim(), ProjectIcon?.type.name));
+      dispatch(setCurrentProjectThunk(inputValue.trim()));
+      setNewProjectModal(false);
+      setInputValue('');
+      setProjectIcon(undefined);
+      toast(`"${inputValue}" project was created`);
+    } else {
+      toast('Field "Name" cannot be empty');
+    }
+  };
+
+  const setIconOnClickHandler = useCallback((icon: JSX.Element) => {
+    setProjectIcon(icon);
+    onActionModal(setShowIconModal, false);
+  }, []);
+
+  const getIcon = useMemo(() => {
+    return icons.map((Icon: IconType) => {
+      return (
+        <button
+          key={Icon.name}
+          className="iconsBox__btn"
+          onClick={() => {
+            const newIcon = <Icon />;
+            setIconOnClickHandler(newIcon);
+          }}
+        >
+          <Icon className="iconsBox__icon" />
+        </button>
+      );
+    });
+  }, [setIconOnClickHandler]);
+
+  const IconModal = (
+    <div className="iconsBox">
+      {allIcons}
+    </div>
+  );
+
+  const NewProjectForm = (
+    <div className="CreateNewProjectForm" id="CreateNewProjectForm">
+      <div className="input-wrapper">
+        <label htmlFor="CreateNewProjectForm__input" className="CreateNewProjectForm__label label">Project Name:</label>
+        <input
+          type="text"
+          id="CreateNewProjectForm__input"
+          className="CreateNewProjectForm__input input"
+          placeholder="My Project"
+          value={inputValue}
+          onChange={onChangeInputHandler}
+          autoFocus
+        />
+      </div>
+      <button
+        type="button"
+        className="CreateNewProjectForm__selectIconBtn button"
+        onClick={() => onActionModal(setShowIconModal, true)}
+      >
+        {ProjectIcon
+          ? ProjectIcon
+          : <span className="CreateNewProjectForm__text">Select Icon</span>}
+      </button>
+    </div>
+  );
+
+  useEffect(() => {
+    setAllIcons(getIcon);
+  }, [getIcon]);
+
+  return (
+    <>
+      <ButtonWithIcon
+        className="CreateNewProject"
+        icon="RiAddLine"
+        text="New Project"
+        onClick={() => onActionModal(setNewProjectModal, true)}
+      />
+      {showNewProjectModal && createPortal(
+        <Modal
+          className="CreateNewProject"
+          isActive={true}
+          onClose={() => onActionModal(setNewProjectModal, false)}
+          onOk={onOkModal}
+          header="Add New Project"
+        >
+          {NewProjectForm}
+        </Modal>,
+        document.body
+      )}
+      {showIconModal && createPortal(
+        <Modal
+          className="IconModal"
+          isActive={true}
+          onClose={() => onActionModal(setShowIconModal, false)}
+          onOk={onOkModal}
+          header="Select Icon"
+          showActionBtns={false}
+        >
+          {IconModal}
+        </Modal>,
+        document.body
+      )}
+    </>
+  );
+}
