c3362ef830aa820cff98e69828ef09830092fbf2
Maxim Predybailo <predmaxim@ya.ru>
2024-01-22 17:19:44 +0500

    refactor(client): optimize imports and code ProjectList component and add id to key prop

--- client/src/components/ProjectList/ProjectList.tsx
+++ client/src/components/ProjectList/ProjectList.tsx
@@ -1,26 +1,28 @@
-import { ButtonWithIcon } from 'components/ButtonWithIcon';
-import { CreateNewProject } from 'components/CreateNewProject';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import { toast } from 'react-toastify';
-import { RootState } from 'store';
-import { ProjectsReducerStateType } from 'store/reducers/projectReducer';
-import { ProjectType, ThunkDispatchType } from 'utils/types';
-import { ROUTES } from '../../router/routes';
-import { deleteProjectThunk } from '../../store/asyncActions/deleteProjectThunk';
-import { setCurrentProjectThunk } from '../../store/asyncActions/setCurrentProjectThunk';
-import { deleteSearch } from '../../store/reducers/searchReducer';
-import { BASE_PROJECT_URL } from '../../utils/constants';
+import {ButtonWithIcon} from 'components/ButtonWithIcon';
+import {CreateNewProject} from 'components/CreateNewProject';
+import {useDispatch, useSelector} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
+import {toast} from 'react-toastify';
+import {RootState} from 'store';
+import {ProjectsReducerStateType} from 'store/reducers/projectReducer';
+import {ProjectType, ThunkDispatchType} from 'utils/types';
+import {ROUTES} from 'router/routes';
+import {deleteProjectThunk} from 'store/asyncActions/deleteProjectThunk';
+import {setCurrentProjectThunk} from 'store/asyncActions/setCurrentProjectThunk';
+import {deleteSearch} from 'store/reducers/searchReducer';
+import {BASE_PROJECT_URL} from 'utils/constants';
 import './ProjectList.scss';
+import React, {useId} from 'react';
 
 export type ProjectListType = {
   isModalAction?: () => void
 }
 
-export function ProjectList({ isModalAction }: ProjectListType) {
+export function ProjectList({isModalAction}: ProjectListType) {
   const dispatch: ThunkDispatchType = useDispatch();
-  const { projects, currentProject }: ProjectsReducerStateType = useSelector((state: RootState) => state.projects);
+  const {projects, currentProject}: ProjectsReducerStateType = useSelector((state: RootState) => state.projects);
   const navigate = useNavigate();
+  const id = useId();
 
   const onClickHandler = (project: ProjectType) => {
     isModalAction && isModalAction();
@@ -43,13 +45,13 @@ export function ProjectList({ isModalAction }: ProjectListType) {
 
   return (
     <div className="ProjectList">
-      <CreateNewProject />
-      <p className="ProjectList__project-header">Select Project:</p>
+      <CreateNewProject/>
+      {!!projects.length && <p className="ProjectList__project-header">Select Project:</p>}
       <div className="ProjectList__container">
         {projects.map((project: ProjectType) => {
           return (
             <ButtonWithIcon
-              key={project._id}
+              key={project._id || id}
               className="ProjectButton"
               icon={project?.icon}
               text={project.name}
