4805b262b82b6ccad5f3ff65b6d8b1f83e743dfe
Maxim Predybailo <predmaxim@ya.ru>
2024-10-07 21:47:33 +0500

    style(server): lint

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -1,7 +1,7 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from '../models/User';
+import User from '../models/user.prisma';
 
 const JWT_SECRET = process.env.JWT_SECRET as string;
 const JWT_EXPIRES = process.env.JWT_EXPIRES as string;
@@ -9,51 +9,51 @@ const JWT_EXPIRES = process.env.JWT_EXPIRES as string;
 // Register user
 export const register = async (req: Request, res: Response) => {
   try {
-    const { username, password } = req.body;
-    const isUsed = await User.findOne({ username });
+    const {username, password} = req.body;
+    const isUsed = await User.findOne({username});
 
     if (isUsed) {
-      return res.status(400).json({ message: 'This username is already busy' });
+      return res.status(400).json({message: 'This username is already busy'});
     }
 
     const salt = bcrypt.genSaltSync(10);
     const hash = bcrypt.hashSync(password, salt);
-    const newUser = new User({ username, password: hash });
-    const token = jwt.sign({ id: newUser._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
+    const newUser = new User({username, password: hash});
+    const token = jwt.sign({id: newUser._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES});
 
     await newUser.save();
 
-    res.status(201).json({ newUser, token, message: 'Registration completed successfully' });
+    res.status(201).json({newUser, token, message: 'Registration completed successfully'});
 
   } catch (error) {
-    res.status(500).json({ message: 'An error when creating a user', error });
+    res.status(500).json({message: 'An error when creating a user', error});
   }
-}
+};
 
 // Login user
 export const login = async (req: Request, res: Response) => {
   try {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
+    const {username, password} = req.body;
+    const user = await User.findOne({username});
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({message: 'User not found'});
     }
 
     const isPasswordCorrect = await bcrypt.compare(password, user.password);
 
     if (!isPasswordCorrect) {
-      return res.status(403).json({ message: 'Incorrect password' });
+      return res.status(403).json({message: 'Incorrect password'});
     }
 
-    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
+    const token = jwt.sign({id: user._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES});
 
-    res.status(200).json({ token, user, message: 'Auth ok' });
+    res.status(200).json({token, user, message: 'Auth ok'});
 
   } catch (error) {
-    res.status(403).json({ message: 'Auth error', error });
+    res.status(403).json({message: 'Auth error', error});
   }
-}
+};
 
 // Get Me
 export const getMe = async (req: Request, res: Response) => {
@@ -61,14 +61,14 @@ export const getMe = async (req: Request, res: Response) => {
     const user = await User.findById(req.query.userId);
 
     if (!user) {
-      return res.status(404).json({ message: 'User not found' });
+      return res.status(404).json({message: 'User not found'});
     }
 
-    const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
+    const token = jwt.sign({id: user._id}, JWT_SECRET, {expiresIn: JWT_EXPIRES});
 
-    res.status(200).json({ user, token });
+    res.status(200).json({user, token});
 
   } catch (error) {
-    res.status(403).json({ message: 'Forbidden', error });
+    res.status(403).json({message: 'Forbidden', error});
   }
-}
+};
--- server/controllers/comments.ts
+++ server/controllers/comments.ts
@@ -1,21 +1,21 @@
-import { Request, Response } from 'express';
-import Comment from '../models/Comment';
-import Task from '../models/Task';
-import { CommentType } from '../utils/types';
+import {Request, Response} from 'express';
+import Comment from '../models/comment.prisma';
+import Task from '../models/models.prisma';
+import {CommentType} from '../utils/types';
 
 export const createComment = async (req: Request, res: Response) => {
   try {
-    const { taskId, comment }: { taskId: string, comment: CommentType } = req.body;
+    const {taskId, comment}: { taskId: string, comment: CommentType } = req.body;
 
     if (!comment)
-      return res.status(400).json({ message: 'Comment cannot be empty' });
+      return res.status(400).json({message: 'Comment cannot be empty'});
 
-    const newComment = new Comment({ comment });
+    const newComment = new Comment({comment});
     await newComment.save();
 
     try {
       await Task.findByIdAndUpdate(taskId, {
-        $push: { comments: newComment._id }
+        $push: {comments: newComment._id}
       });
     } catch (error) {
       console.log(error);
@@ -24,6 +24,6 @@ export const createComment = async (req: Request, res: Response) => {
     res.status(201).json(newComment);
 
   } catch (error) {
-    res.status(500).json({ message: 'Something went wrong', error });
+    res.status(500).json({message: 'Something went wrong', error});
   }
 };
