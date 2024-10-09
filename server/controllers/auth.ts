3bea4a9c930ebc663074196e3907295f0f44944b
Maxim Predybailo <predmaxim@ya.ru>
2024-10-09 13:02:28 +0500

    feat(server): add zod validation arguments in controllers

--- server/controllers/auth.ts
+++ server/controllers/auth.ts
@@ -1,7 +1,8 @@
-import {Request, RequestHandler, Response} from 'express';
-import {compareSync, genSaltSync, hashSync} from 'bcrypt-ts';
+import { Request, RequestHandler, Response } from 'express';
+import { compareSync, genSaltSync, hashSync } from 'bcrypt-ts';
 import jwt from 'jsonwebtoken';
-import {prisma} from '@/lib/prisma-client';
+import { prisma } from '@/lib/prisma-client';
+import { z } from 'zod';
 
 const JWT_SECRET = process.env.JWT_SECRET || '';
 const JWT_EXPIRES = process.env.JWT_EXPIRES || '15m';
@@ -9,15 +10,20 @@ const JWT_EXPIRES = process.env.JWT_EXPIRES || '15m';
 // Register user
 export const register: RequestHandler = async (req: Request, res: Response) => {
   try {
-    const {email, password} = req.body as { email: string, password: string };
-    const isUsed = await prisma.user.findFirst({where: {email}});
+    const { email, password } = z.object({
+      email: z.string().email(),
+      password: z.string()
+    }).parse(req.body);
+
+    const isUsed = await prisma.user.findFirst({ where: { email } });
 
     if (isUsed) {
-      res.status(400).json({message: 'This email is already busy'});
+      res.status(400).json({ message: 'This email is busy', ok: false });
+      return;
     }
 
-    const salt = genSaltSync(10);
-    const hash = hashSync(password, salt);
+    const hash = hashSync(password, genSaltSync(10));
+
     const newUser = await prisma.user.create({
       data: {
         password: hash,
@@ -25,64 +31,63 @@ export const register: RequestHandler = async (req: Request, res: Response) => {
         roleId: 2
       }
     });
-    const token = jwt.sign({id: newUser.id}, JWT_SECRET, {expiresIn: JWT_EXPIRES});
 
-    res.status(201).json({newUser, token, message: 'Registration completed successfully'});
+    const token = jwt.sign(
+      { id: newUser.id },
+      JWT_SECRET,
+      { expiresIn: JWT_EXPIRES }
+    );
 
+    res.status(201).json({ newUser, token, message: 'Registration completed successfully', ok: true });
   } catch (error) {
-    res.status(500).json({message: 'An error when creating a user', error});
+    res.status(500).json({ message: 'An error when creating a user', ok: false , error });
   }
 };
 
 // Login user
 export const login: RequestHandler = async (req: Request, res: Response) => {
   try {
-    const {email, password} = req.body;
-    const user = await prisma.user.findFirst({where: {email}});
+    const { email, password } = z.object({
+      email: z.string().email(),
+      password: z.string()
+    }).parse(req.body);
+
+    const user = await prisma.user.findFirst({ where: { email } });
 
     if (!user) {
-      res.status(404).json({message: 'User not found'});
+      res.status(404).json({ message: 'User not found', ok: false });
       return;
     }
 
+    // TODO: check, may be password need to be hashed before compare
     const isPasswordCorrect = compareSync(password, user.password);
 
     if (!isPasswordCorrect) {
-      res.status(403).json({message: 'Incorrect password'});
+      res.status(403).json({ message: 'Incorrect password' });
       return;
     }
 
-    const token = jwt.sign({id: user.id}, JWT_SECRET, {expiresIn: JWT_EXPIRES});
-
-    res.status(200).json({token, user, message: 'Auth ok'});
-
+    const token = jwt.sign({ id: user.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
+    res.status(200).json({ token, user, message: 'Auth success!', ok: true });
   } catch (error) {
-    res.status(403).json({message: 'Auth error', error});
+    res.status(403).json({ message: 'Auth error!', error, ok: false });
   }
 };
 
 // Get Me
 export const getMe: RequestHandler = async (req: Request, res: Response) => {
   try {
-    const id = req.query.userId ? Number(req.query.userId) : null;
-
-    if (!id) {
-      res.status(400).json({message: 'Bad payload'});
-      return;
-    }
-
-    const user = await prisma.user.findUnique({where: {id}});
+    const userId = z.object({ userId: z.number() }).parse(req.query).userId;
+    const user = await prisma.user.findUnique({ where: { id: userId } });
 
     if (!user) {
-      res.status(404).json({message: 'User not found'});
+      res.status(404).json({ message: 'User not found!', ok: false });
       return;
     }
 
-    const token = jwt.sign({id: user?.id}, JWT_SECRET, {expiresIn: JWT_EXPIRES});
-
-    res.status(200).json({user, token});
-
+    const token = jwt.sign({ id: user?.id }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
+    res.status(200).json({ user, token, ok: true });
   } catch (error) {
-    res.status(403).json({message: 'Forbidden', error});
+    res.status(403).json({ message: 'Forbidden!', error, ok: false });
   }
 };
