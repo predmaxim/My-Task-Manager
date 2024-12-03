b7355ca105dce58dc6e4a5ec15a80c59dbae3a35
Maxim Predybailo <predmaxim@ya.ru>
2024-12-03 21:51:49 +0000

    feat: add header, styles and classes

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,11 +1,19 @@
+import { Header } from 'components';
+import { RiArrowLeftLine } from 'react-icons/ri'
 import './NotFoundPage.scss';
 
 export default function NotFoundPage() {
   return (
-    <main className="notFoundPage">
-      <div className="container">
-        <h1>404 Page not found</h1>
-      </div>
-    </main>
+    <>
+      <Header />
+      <main className="NotFoundPage">
+        <div className="container">
+          <h1 className="NotFoundPage__header">404 Page not found</h1>
+          <div className="NotFoundPage__link">
+            <RiArrowLeftLine /><a href="/">Go to Home</a>
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
