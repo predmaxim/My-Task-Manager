54ee6f020584cd9d42b4b8dbfb0d508edce5f8cc
Maxim Predybailo <predmaxim@ya.ru>
2024-12-05 18:18:14 +0000

    feat(client): add auth page

---
+++ client/src/pages/auth/AuthPage.tsx
@@ -0,0 +1,17 @@
+import { MainHeader } from 'src/components/MainHeader';
+import './AuthPage.scss';
+
+export default function AuthPage() {
+  return (
+    <>
+      <MainHeader />
+      <main className="ProjectsPage">
+        <div className="container">
+          <h1>AuthPage Page</h1>
+
+      
+        </div>
+      </main>
+    </>
+  );
+}
