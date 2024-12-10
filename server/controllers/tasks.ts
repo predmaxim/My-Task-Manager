f820bf8e2aaacc8053dd5a87ce2e25977e9ecda0
Maxim Predybailo <predmaxim@ya.ru>
2024-12-10 12:46:14 +0000

    feat(server): change get task controller logic based on project

--- server/controllers/tasks.ts
+++ server/controllers/tasks.ts
@@ -1,15 +1,13 @@
 // import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
-import { TaskType } from '../types';
+import { ProjectType, TaskType } from '../types';
 import Task from '../models/Task';
-
-const JWT_SECRET = process.env.JWT_SECRET as string;
-const JWT_EXPIRES = process.env.JWT_EXPIRES as string;
 
 export const getTasks = async (req: Request, res: Response) => {
   try {
-    const tasks = await Task.find<TaskType[]>().sort('-created');
-
+    const projectName = req.params.projectName;
+    const tasks = await Task.find<TaskType[]>({ project: projectName }).sort('created');
+    
     if (!tasks.length) {
       return res.status(404).json({ message: 'There are no tasks' });
     }
@@ -28,8 +26,6 @@ export const getTask = async (req: Request, res: Response) => {
     if (!task) {
       return res.status(404).json({ message: 'Task not found' });
     }
-
-    // const token = jwt.sign({ id: task._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
 
     res.status(200).json({ task });
 
@@ -69,7 +65,6 @@ export const deleteTask = async (req: Request, res: Response) => {
 
     if (task) {
       const removedTask = await Task.findByIdAndDelete<TaskType>(req.params.id);
-      // const token = jwt.sign({ id: task._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
       res.status(200).json({ removedTask });
     } else {
       return res.status(404).json({ message: 'Task not found' });
@@ -87,7 +82,6 @@ export const updateTask = async (req: Request, res: Response) => {
 
     if (task) {
       const updatedTask = await Task.findByIdAndUpdate<TaskType>(req.params.id, req.body);
-      // const token = jwt.sign({ id: task._id }, JWT_SECRET, { expiresIn: JWT_EXPIRES });
       res.status(200).json({ updatedTask });
     } else {
       return res.status(404).json({ message: 'Task not found' });
