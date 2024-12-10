60f575ed25c68a67316545d9f52ca97d9c280600
Maxim Predybailo <predmaxim@ya.ru>
2024-12-10 15:52:14 +0000

    feat(client): reconfigure router

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -1,17 +1,33 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { TasksPage } from 'src/pages/TasksPage';
-import { ProjectsPage } from 'src/pages/ProjectsPage';
-import { NotFoundPage } from 'src/pages/NotFoundPage';
+import { TasksPage } from 'src/pages';
+import { ProjectsPage } from 'src/pages';
+import { NotFoundPage } from 'src/pages';
+import { HomeLayout, Layout } from 'src/components/Layouts';
 import { ROUTES } from './routes';
+import { ToastContainer } from 'react-toastify';
 
 export function Router() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path={ROUTES.projects} element={<ProjectsPage />} />
-        <Route path={ROUTES.tasks} element={<TasksPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<HomeLayout />} >
+            <Route path={ROUTES.projects} element={<ProjectsPage />} />
+          </Route>
+          <Route element={<Layout />} >
+            <Route path={ROUTES.tasks} element={<TasksPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar
+        newestOnTop={false}
+        closeOnClick
+        theme="light"
+      />
+    </>
   );
 }
--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -1,4 +1,4 @@
 export const ROUTES = {
   projects: '/',
-  tasks: 'tasks'
-};
+  tasks: 'project/:name'
+}
