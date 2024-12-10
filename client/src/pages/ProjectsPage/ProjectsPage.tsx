4fc803fa203b6e28574aee6c954fff49c163e9aa
Maxim Predybailo <predmaxim@ya.ru>
2024-12-10 10:50:26 +0000

    feat(client): change pages based on new router

--- client/src/pages/ProjectsPage/ProjectsPage.tsx
+++ client/src/pages/ProjectsPage/ProjectsPage.tsx
@@ -1,20 +1,15 @@
-import { HomeHeader } from 'src/components';
 import { ProjectList } from 'src/components';
 import { NewProject } from 'src/components';
 import './ProjectsPage.scss';
 
 export default function ProjectsPage() {
   return (
-    <>
-      <HomeHeader />
       <main className="ProjectsPage">
         <div className="container">
           <NewProject />
           <p className="ProjectsPage__project-header">Select Project:</p>
           <ProjectList />
         </div>
-
       </main>
-    </>
   );
-}
+};
