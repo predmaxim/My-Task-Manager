d8fdb7d9a81192c3a60644dfc2e61182cc876062
Maxim Predybailo <predmaxim@ya.ru>
2024-12-12 22:13:18 +0000

    feat(server): change models fields based on new types

--- server/models/Project.ts
+++ server/models/Project.ts
@@ -1,23 +1,28 @@
-import mongoose from 'mongoose';
-import { ProjectType } from '../utils/types';
-import { TaskSchema } from './Task';
+import mongoose from "mongoose";
+import { ProjectType } from "../utils/types";
+import { TaskSchema } from "./Task";
 
 export const ProjectSchema = new mongoose.Schema<ProjectType>({
     name: {
-        type: String,
-        maxLength: [50, 'Your project name must be shorter than 50 characters'],
-        minLength: [1, 'Your project name must be longer than 0 characters'],
+      type: String,
+      maxLength: [50, "Your project name must be shorter than 50 characters"],
+      minLength: [1, "Your project name must be longer than 0 characters"],
+      unique: true,
     },
     status: String,
     created: Date,
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    current: Boolean,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
     icon: String,
     color: String,
     tasks: {
-        type: [TaskSchema],
-        default: []
+      type: [TaskSchema],
+      default: [],
     },
-}
+  },
 );
 
-export default mongoose.models.Project || mongoose.model<ProjectType>('Project', ProjectSchema);
+export default mongoose.models.Project || mongoose.model<ProjectType>("Project", ProjectSchema);
--- server/models/Task.ts
+++ server/models/Task.ts
@@ -1,35 +1,50 @@
-import mongoose from 'mongoose';
+import { model, models, Schema } from 'mongoose';
 import { TaskType } from '../utils/types';
 import { CommentSchema } from './Comment';
 
-export const TaskSchema = new mongoose.Schema<TaskType>({
-  number: Number,
-  name: {
-    type: String,
-    minLength: [1, 'Your project name must be longer than 0 characters'],
-    maxLength: [50, 'Your project name must be shorter than 50 characters'],
-  },
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  project: { type: mongoose.Schema.Types.String, ref: 'Project' },
-  created: Date,
-  description: {
-    type: String,
-    minLength: [1, 'Your description must be longer than 0 characters'],
-    maxLength: [1000, 'Your description must be shorter than 1000 characters'],
-  },
-  done: Date,
-  priority: String,
-  status: String,
-  lastStatus: String,
-  parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Task' },
-  due: Date,
-  inWork: Date,
-  files: [String],
-  comments: {
-    type: [CommentSchema],
-    default: {}
-  },
-}
-);
+export const TaskSchema =
+  new Schema<TaskType>({
+      number: {
+        type: Number,
+        required: true
+      },
+      name: {
+        type: String,
+        minLength: [1, 'Your project name must be longer than 0 characters'],
+        maxLength: [100, 'Your project name must be shorter than 50 characters'],
+        required: true
+      },
+      user: { type: Schema.Types.ObjectId, ref: 'User' },
+      project: {
+        type: Schema.Types.String,
+        ref: 'Project',
+        required: true
+      },
+      created: {
+        type: Date,
+        default: Date.now()
+      },
+      description: {
+        type: String,
+        minLength: [1, 'Your description must be longer than 0 characters'],
+        maxLength: [1000, 'Your description must be shorter than 1000 characters']
+      },
+      done: Schema.Types.Mixed,
+      priority: String,
+      status: String,
+      lastStatus: String,
+      parent: {
+        type: Schema.Types.ObjectId,
+        ref: 'Task'
+      },
+      due: Schema.Types.Mixed,
+      inWork: Schema.Types.Mixed,
+      files: [String],
+      comments: {
+        type: [CommentSchema],
+        default: {}
+      }
+    }
+  );
 
-export default mongoose.models.Task || mongoose.model<TaskType>('Task', TaskSchema);
+export default models.Task || model<TaskType>('Task', TaskSchema);
