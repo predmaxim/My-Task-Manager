e7c0d6780004442522e6e01b74928abe47a35e89
Maxim Predybailo <predmaxim@ya.ru>
2024-12-13 21:42:01 +0000

    feat(client): add global provider and move toast to it from router

--- client/src/router/router.tsx
+++ client/src/router/router.tsx
@@ -1,33 +1,22 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { TasksPage } from 'src/pages';
-import { ProjectsPage } from 'src/pages';
-import { NotFoundPage } from 'src/pages';
-import { HomeLayout, Layout } from 'src/components/Layouts';
+import { HomeLayout, Layout } from 'components/Layouts';
+import { NotFoundPage, ProjectsPage, TasksPage } from 'pages';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ROUTES } from './routes';
-import { ToastContainer } from 'react-toastify';
 
 export function Router() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route element={<HomeLayout />} >
+          <Route element={<HomeLayout />}>
             <Route path={ROUTES.projects} element={<ProjectsPage />} />
           </Route>
-          <Route element={<Layout />} >
+          <Route element={<Layout />}>
             <Route path={ROUTES.tasks} element={<TasksPage />} />
-            <Route path="*" element={<NotFoundPage />} />
           </Route>
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </BrowserRouter>
-      <ToastContainer
-        position="top-right"
-        autoClose={3000}
-        hideProgressBar
-        newestOnTop={false}
-        closeOnClick
-        theme="light"
-      />
     </>
   );
 }
