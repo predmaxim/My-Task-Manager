1daec0b788f283a25177000399cd2ba51c37ab3c
Maxim Predybailo <predmaxim@ya.ru>
2024-12-13 16:22:45 +0000

    feat(client): add store

---
+++ client/src/store/store.ts
@@ -0,0 +1,32 @@
+import { composeWithDevTools } from '@redux-devtools/extension';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { Action, applyMiddleware, combineReducers, legacy_createStore as createStore } from 'redux';
+import thunk, { ThunkAction } from 'redux-thunk';
+import { commentReducer } from './reducers/commentReducer';
+import { projectReducer } from './reducers/projectReducer';
+import { taskReducer } from './reducers/taskReducer';
+
+const rootReducer = combineReducers({
+  projects: projectReducer,
+  tasks: taskReducer,
+  comments: commentReducer
+});
+
+const composeEnhancers = composeWithDevTools({ trace: true });
+
+export const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof createStore>;
+export type AppDispatch = AppStore['dispatch'];
+export type AppThunk<ReturnType = void> = ThunkAction<
+  ReturnType,
+  RootState,
+  unknown,
+  Action<string>
+>;
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
