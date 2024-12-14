20198cab7e280309158d6a6e0de767dc3cddc1fa
Maxim Predybailo <predmaxim@ya.ru>
2024-12-14 12:43:02 +0000

    feat(client): add logics and styles to ButtonWithIcon component

--- client/src/components/ButtonWithIcon/ButtonWithIcon.tsx
+++ client/src/components/ButtonWithIcon/ButtonWithIcon.tsx
@@ -1,24 +1,40 @@
-import { useMemo } from 'react';
-import { icons } from 'src/components/icons';
+import { icons } from 'components/icons';
+import { MouseEvent } from 'react';
 import './ButtonWithIcon.scss';
 
 export type ButtonWithIconProps = {
   className: string,
   icon?: string,
   text?: string,
-  onClick: () => void
+  onClick: (e: MouseEvent<HTMLButtonElement>) => void,
+  showActions?: boolean,
+  actions?: {
+    remove?: () => void,
+    edit?: () => void
+  }
 }
 
-export function ButtonWithIcon({ className, icon, text, onClick }: ButtonWithIconProps) {
-  const BtnIcon = useMemo(() => icons.filter((el) => el.name === icon)[0], [icon]);
+export function ButtonWithIcon({ className, icon, text, onClick, showActions = false, actions }: ButtonWithIconProps) {
+  const GenerateBtnIcon = (icon: string) => icons.filter((el) => el.name === icon)[0];
+
+  const BtnIcon = GenerateBtnIcon(icon ? icon : '');
+  const BtnIconRemove = GenerateBtnIcon('RiDeleteBin6Line');
+  const BtnIconEdit = GenerateBtnIcon('RiPencilLine');
 
   return (
-    <button className={`ButtonWithIcon button ${className}`} onClick={onClick}>
-      {
-        icon &&
-        <BtnIcon className="ButtonWithIcon__icon" />
-      }
-      <span className="ButtonWithIcon__text">{text}</span>
-    </button >
-  );
+    <div className={`ButtonWithIconWrap`}>
+      <button
+        className={`ButtonWithIcon button ${className}`}
+        onClick={onClick}>
+        {icon
+          && icon.toLowerCase() !== 'loading...'
+          && <BtnIcon className="ButtonWithIcon__icon" />}
+        {text && <span className="ButtonWithIcon__text">{text}</span>}
+      </button>
+      {showActions &&
+        <div className="button__actions">
+          <BtnIconRemove className="ButtonWithIcon__icon button" onClick={actions && actions.remove} />
+          <BtnIconEdit className="ButtonWithIcon__icon button" onClick={actions && actions.edit} />
+        </div>}
+    </div>);
 }
