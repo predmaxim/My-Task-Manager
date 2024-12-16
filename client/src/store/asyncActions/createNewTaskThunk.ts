017ec7fddb1258092b50e02815eb63e998fd36d6
Maxim Predybailo <predmaxim@ya.ru>
2024-12-16 09:25:01 +0000

    feat(client): optimize task creation

--- client/src/store/asyncActions/createNewTaskThunk.ts
+++ client/src/store/asyncActions/createNewTaskThunk.ts
@@ -1,21 +1,19 @@
 import axios from 'axios';
 import { Dispatch } from 'redux';
-import { createTask } from 'store/reducers/taskReducer';
+import { createTask, updateTask } from 'store/reducers/taskReducer';
 import { BASE_URL } from 'utils/constants';
 import { TaskType } from 'utils/types';
 
 export const createNewTaskThunk = (task: TaskType) => {
   return async (dispatch: Dispatch) => {
     try {
+      dispatch(createTask(task));
       const { data }: {
         data: {
           task: TaskType
         }
       } = await axios.post(`${BASE_URL}/api/tasks`, task);
-      dispatch(createTask(data.task));
-
-
-      // dispatch(updateProject(data.task.project as UpdateProjectsPayloadType));
+      dispatch(updateTask(data.task));
 
     } catch (error) {
       console.log(`Create Tasks Error: ${error}`);
