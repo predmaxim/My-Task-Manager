7329ed0c5da63d1c9b5671932220895708033bc5
Maxim Predybailo <predmaxim@ya.ru>
2024-12-17 11:18:11 +0000

    feat(client): add dnd and change some logic on Board

--- client/src/components/Board/Board.tsx
+++ client/src/components/Board/Board.tsx
@@ -1,17 +1,17 @@
 import { ButtonWithIcon } from 'components/ButtonWithIcon';
 import { CreateNewTask } from 'components/CreateNewTask/CreateNewTask';
 import { Task } from 'components/Task';
-import { nanoid } from 'nanoid';
-import { useCallback, useLayoutEffect } from 'react';
+import { useCallback, useLayoutEffect, useState } from 'react';
+import { DragDropContext, Draggable, Droppable, DropResult } from 'react-beautiful-dnd';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store';
 import { getTasksFromDbThunk } from 'store/asyncActions/getTasksFromDbThunk';
-import { TaskReducerStateType } from 'store/reducers/taskReducer';
-import { TASK_STATUSES } from 'utils/constants';
+import { setTasks, TaskReducerStateType } from 'store/reducers/taskReducer';
 import { upperCaseFirstLetter } from 'utils/helpers';
 import { TaskStatusType, TaskType, ThunkDispatchType } from 'utils/types';
+import { updateTaskThunk } from '../../store/asyncActions/updateTaskThunk';
 import { SearchReducerStateType } from '../../store/reducers/searchReducer';
-import { Loading } from '../Loading';
+import { TASK_STATUSES } from '../../utils/constants';
 import './Board.scss';
 
 export type BoardType = {
@@ -19,76 +19,153 @@ export type BoardType = {
 }
 
 export type ColumnType = {
+  id: TaskStatusType,
   title: TaskStatusType,
   onClickMenu: () => void,
-  currentColumnTasks: TaskType[],
+  tasks: TaskType[],
   total: number
 }
 
 export function Board({ currentProjectName }: BoardType) {
   const dispatch: ThunkDispatchType = useDispatch();
-  const { tasks, isLoading }: TaskReducerStateType = useSelector((state: RootState) => state.tasks);
+  const { tasks }: TaskReducerStateType = useSelector((state: RootState) => state.tasks);
   const { query }: SearchReducerStateType = useSelector((state: RootState) => state.search);
+  const [board, setBoard] = useState<ColumnType[]>([]);
 
-  const genColumn = (status: TaskStatusType): ColumnType => {
+  const genColumn = useCallback((status: TaskStatusType): ColumnType => {
     const newQuery = new RegExp(query.toLowerCase());
     const tasksByStatus: TaskType[] = tasks.filter(
       (task: TaskType) => task.status === status
     );
 
     return {
+      id: status,
       title: status,
       onClickMenu: () => console.log('menu'),
-      currentColumnTasks: tasksByStatus.filter((task: TaskType) =>
+      tasks: tasksByStatus.filter((task: TaskType) =>
         task.name.toLowerCase().match(newQuery)
         || String(task.number).toLowerCase().match(newQuery)),
       total: tasksByStatus.length
     };
-  };
+  }, [query, tasks]);
 
-  const columns: ColumnType[] = (Object.keys(TASK_STATUSES) as TaskStatusType[])
-    .map((status: TaskStatusType) => genColumn(status));
+  const genBoard = useCallback(() => {
+    const newColumns = (Object.keys(TASK_STATUSES) as TaskStatusType[])
+      .map((status: TaskStatusType) => genColumn(status));
+    setBoard(newColumns);
+  }, [genColumn]);
+
+  const onDragEnd = (result: DropResult) => {
+    const {
+      source,
+      destination
+    } = result;
+    if (!destination) return;
+    if (source.droppableId === destination.droppableId
+      && source.index === destination.index) return;
+
+    const newBoard = [...board];
+
+    const columnSourceIndex = newBoard
+      .findIndex(column => column.id === source.droppableId);
+    const columnDestinationIndex = newBoard
+      .findIndex(column => column.id === destination.droppableId);
+
+    const taskSourceIndex = source.index;
+    const taskDestinationIndex = destination.index;
+
+
+    const sourceColumn: ColumnType = {
+      ...newBoard[columnSourceIndex]
+    };
+    const destinationColumn: ColumnType = {
+      ...newBoard[columnDestinationIndex]
+    };
+
+    const [removedTask] = sourceColumn.tasks
+      .splice(taskSourceIndex, 1);
+
+    if (source.droppableId !== destination.droppableId) {
+      removedTask.done = destination.droppableId === TASK_STATUSES.done
+        ? new Date
+        : false;
+      removedTask.status = destination.droppableId as TaskStatusType;
+      removedTask.lastStatus = destination.droppableId === TASK_STATUSES.done
+        ? source.droppableId as TaskStatusType
+        : TASK_STATUSES.queue as TaskStatusType;
+    }
+
+    destinationColumn.tasks.splice(taskDestinationIndex, 0, removedTask);
+
+    setBoard(newBoard);
+
+    const newTasks = newBoard
+      .map((column) => column.tasks)
+      .flat();
+
+    dispatch(setTasks(newTasks));
+    dispatch(updateTaskThunk(removedTask));
+  };
 
   const getAllTasks = useCallback(
     () => {
       currentProjectName && dispatch(getTasksFromDbThunk(currentProjectName));
-    }, [currentProjectName, dispatch]);
+    }, [currentProjectName, dispatch]
+  );
 
+  useLayoutEffect(() => genBoard(), [genBoard]);
   useLayoutEffect(() => getAllTasks(), [getAllTasks]);
 
   return (
     <div className={`Board`}>
-      {columns.map(({ title, onClickMenu, currentColumnTasks }: ColumnType) => {
-        return (
-          <div className={`column column-${title}`} key={title}>
-            <div className="column__header">
-              <div className="column__title">{upperCaseFirstLetter(title)}</div>
-              <ButtonWithIcon
-                className="column__menuBtn"
-                onClick={onClickMenu}
-                icon="RiMore2Line"
-              />
+      <DragDropContext key={'Board'} onDragEnd={onDragEnd}>
+        {board.map(({ id, title, onClickMenu, tasks }: ColumnType) => {
+          return (
+            <div className={`column column-${title}`} key={`column-${id}`}>
+              <div className="column__header">
+                <div className="column__title">{upperCaseFirstLetter(title)}</div>
+                <ButtonWithIcon
+                  className="column__menuBtn"
+                  onClick={onClickMenu}
+                  icon="RiMore2Line"
+                />
+              </div>
+              <div className="column__CreateNewTask">
+                {currentProjectName &&
+                  <CreateNewTask
+                    projectName={currentProjectName}
+                    taskStatus={title}
+                  />}
+              </div>
+              <Droppable droppableId={id} type="column">
+                {(provided) => (
+                  <div className="column__body" {...provided.droppableProps} ref={provided.innerRef}>
+                    {tasks.map((task: TaskType, i) => (
+                      <Draggable
+                        draggableId={`${task.number}`}
+                        key={task.number}
+                        index={i}
+                      >
+                        {(provided) => (
+                          <div
+                            key={task.number}
+                            {...provided.dragHandleProps}
+                            {...provided.draggableProps}
+                            ref={provided.innerRef}
+                          >
+                            <Task task={task} />
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
             </div>
-            <div className="column__CreateNewTask">
-              {currentProjectName &&
-                <CreateNewTask
-                  projectName={currentProjectName}
-                  taskStatus={title}
-                />}
-            </div>
-            <div className="column__body">
-              {isLoading
-                ? <Loading />
-                : currentColumnTasks.map((task: TaskType) =>
-                  <Task
-                    task={task}
-                    key={nanoid()}
-                  />)
-              }
-            </div>
-          </div>
-        );
-      })}
+          );
+        })}
+      </DragDropContext>
     </div>
   );
 }
