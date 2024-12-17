66f7849e8ff7cbf06563bb17d6c3ac95440ecbbf
Maxim Predybailo <predmaxim@ya.ru>
2024-12-17 10:37:12 +0000

    feat(client): turn on set current project in tasks page

--- client/src/pages/TasksPage/TasksPage.tsx
+++ client/src/pages/TasksPage/TasksPage.tsx
@@ -1,24 +1,28 @@
 import { Board } from 'components';
+import { useLayoutEffect } from 'react';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { RootState } from '../../store';
+import { setCurrentProjectThunk } from '../../store/asyncActions/setCurrentProjectThunk';
 import { ProjectsReducerStateType } from '../../store/reducers/projectReducer';
 import { APP_NAME } from '../../utils/constants';
-import { ThunkDispatchType } from '../../utils/types';
+import { ProjectType, ThunkDispatchType } from '../../utils/types';
 import './TasksPage.scss';
 
 export function TasksPage() {
   const { projects }: ProjectsReducerStateType = useSelector((state: RootState) => state.projects);
   const { name: projectName } = useParams();
   const dispatch: ThunkDispatchType = useDispatch();
+  const navigate = useNavigate();
 
-  // useLayoutEffect(() => {
-  //   const isAvailable = projects.some((project: ProjectType) => project.name === projectName);
-  //   if (isAvailable && projectName) {
-  //     dispatch(setCurrentProjectThunk(projectName));
-  //   }
-  // }, [projectName, dispatch, projects]);
+  useLayoutEffect(() => {
+    const isAvailable = projects.some((project: ProjectType) => project.name === projectName);
+    if (isAvailable && projectName) {
+      dispatch(setCurrentProjectThunk(projectName));
+    }
+    // else navigate(ROUTES.projects);
+  }, [projectName, dispatch, projects, navigate]);
 
   return (
     <HelmetProvider>
