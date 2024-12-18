0141fed41059f4a91a139aa8f50d5e666b3e1578
Maxim Predybailo <predmaxim@ya.ru>
2024-12-18 14:21:36 +0000

    refactor(client): optimize code CreateNewTask component and remove lastStatus field

--- client/src/components/CreateNewTask/CreateNewTask.tsx
+++ client/src/components/CreateNewTask/CreateNewTask.tsx
@@ -1,46 +1,42 @@
-import { ButtonWithIcon } from 'components/ButtonWithIcon';
-import { FocusEvent, KeyboardEvent, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, {FocusEvent, KeyboardEvent, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
 import TextareaAutosize from 'react-textarea-autosize';
-import { RootState } from '../../store';
-import { createNewTaskThunk } from '../../store/asyncActions/createNewTaskThunk';
-import { ProjectsReducerStateType } from '../../store/reducers/projectReducer';
-import { TASK_STATUSES, TEMP_USER } from '../../utils/constants';
-import { TaskStatusType, TaskType, ThunkDispatchType } from '../../utils/types';
+import {RootState} from '../../store';
+import {createNewTaskThunk} from '../../store/asyncActions/createNewTaskThunk';
+import {ProjectsReducerStateType} from '../../store/reducers/projectReducer';
+import {TASK_STATUSES, TEMP_USER} from '../../utils/constants';
+import {TaskStatusType, TaskType, ThunkDispatchType} from '../../utils/types';
+import {ButtonWithIcon} from 'components/ButtonWithIcon';
 import './CreateNewTask.scss';
 
 export type CreateNewTaskType = {
-  projectName: string,
-  taskStatus: TaskStatusType
-}
+  projectName: string;
+  taskStatus: TaskStatusType;
+};
 
-export function CreateNewTask({ projectName, taskStatus }: CreateNewTaskType) {
+export function CreateNewTask({projectName, taskStatus}: CreateNewTaskType) {
   const dispatch: ThunkDispatchType = useDispatch();
-  const { currentProject }: ProjectsReducerStateType = useSelector((state: RootState) => state.projects);
-  const [newTaskName, setNewTaskName] = useState<string>();
+  const {currentProject}: ProjectsReducerStateType = useSelector((state: RootState) => state.projects);
+
+  const [newTaskName, setNewTaskName] = useState<string>('');
   const [showTaskCreator, setShowTaskCreator] = useState(false);
 
   const createNewTask = () => {
     if (currentProject) {
       const newTask: TaskType = {
-        name: newTaskName || '',
+        name: newTaskName,
         project: projectName,
         user: TEMP_USER,
         status: taskStatus,
-        lastStatus: taskStatus === TASK_STATUSES.done as TaskStatusType
-          ? TASK_STATUSES.queue as TaskStatusType
-          : taskStatus,
-        done: taskStatus === TASK_STATUSES.done as TaskStatusType
-          ? new Date()
-          : undefined,
+        done: taskStatus === TASK_STATUSES.done ? new Date() : undefined,
         comments: [],
         created: new Date(),
         priority: 'low',
         number: currentProject.tasks + 1,
         index: 0
       };
-      dispatch(createNewTaskThunk(newTask as TaskType));
-      setNewTaskName(undefined);
+      dispatch(createNewTaskThunk(newTask));
+      setNewTaskName('');
       setShowTaskCreator(false);
     }
   };
@@ -48,17 +44,16 @@ export function CreateNewTask({ projectName, taskStatus }: CreateNewTaskType) {
   const onClickNewBtn = () => setShowTaskCreator(true);
 
   const onKeyDownTaskName = (e: KeyboardEvent<HTMLTextAreaElement>) => {
-    if ((e.code === 'Enter' || e.code === 'NumpadEnter')) {
+    if (e.code === 'Enter' || e.code === 'NumpadEnter') {
       newTaskName ? createNewTask() : setShowTaskCreator(false);
     }
     if (e.code === 'Escape') {
       setShowTaskCreator(false);
-      setNewTaskName(undefined);
+      setNewTaskName('');
     }
   };
 
-  const onBlurNewTaskCreator = () =>
-    newTaskName ? createNewTask() : setShowTaskCreator(false);
+  const onBlurNewTaskCreator = () => (newTaskName ? createNewTask() : setShowTaskCreator(false));
 
   const onChangeHandler = (e: FocusEvent<HTMLTextAreaElement>) =>
     setNewTaskName(e.currentTarget.value);
@@ -71,7 +66,7 @@ export function CreateNewTask({ projectName, taskStatus }: CreateNewTaskType) {
         icon="RiAddLine"
         text="New Task"
       />
-      {showTaskCreator &&
+      {showTaskCreator && (
         <TextareaAutosize
           className="CreateNewTask__newTaskText"
           onBlur={onBlurNewTaskCreator}
@@ -79,7 +74,8 @@ export function CreateNewTask({ projectName, taskStatus }: CreateNewTaskType) {
           onChange={onChangeHandler}
           value={newTaskName}
           autoFocus={true}
-        />}
+        />
+      )}
     </div>
   );
 }
