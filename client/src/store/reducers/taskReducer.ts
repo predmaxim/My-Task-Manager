3288c0429254c9115b635056a7253e41711ab0a3
Maxim Predybailo <predmaxim@ya.ru>
2024-12-19 13:18:45 +0000

    refactor(client): optimize reducers:
    - task reducer
    - project reducer

--- client/src/store/reducers/taskReducer.ts
+++ client/src/store/reducers/taskReducer.ts
@@ -1,96 +1,70 @@
-import { TaskType } from 'utils/types';
+import {TaskType} from 'utils/types';
 
 export type TaskReducerStateType = {
-  tasks: TaskType[],
-  isLoading: boolean
-}
+  tasks: TaskType[];
+  isLoading: boolean;
+};
 
-type SET_TASKS_ActionType = {
-  type: string,
-  payload: TaskType[]
-}
-
-type CREATE_TASK_ActionType = {
-  type: string,
-  payload: TaskType
-}
-
-type UPDATE_TASK_ActionType = {
-  type: string,
-  payload: TaskType
-}
-
-type DELETE_TASK_ActionType = {
-  type: string,
-  payload: TaskType['_id']
-}
-type SET_LOADING_ActionType = {
-  type: string,
-  payload: boolean
-}
-
-export type TaskActionType =
-  SET_TASKS_ActionType
-  | CREATE_TASK_ActionType
-  | UPDATE_TASK_ActionType
-  | DELETE_TASK_ActionType
-  | SET_LOADING_ActionType
-
+type TaskActionType<T, P> = {
+  type: T;
+  payload: P;
+};
 
 export const initialState: TaskReducerStateType = {
   tasks: [],
   isLoading: true
 };
 
-// actions
-export const SET_TASKS = 'SET_TASKS';
-export const CREATE_TASK = 'CREATE_TASK';
-export const UPDATE_TASK = 'UPDATE_TASK';
-export const DELETE_TASK = 'DELETE_TASK';
-export const SET_LOADING = 'SET_LOADING';
+const createActionType = <T extends string>(action: T) => action;
 
-// reducers
+const SET_TASKS = createActionType('SET_TASKS');
+const CREATE_TASK = createActionType('CREATE_TASK');
+const UPDATE_TASK = createActionType('UPDATE_TASK');
+const DELETE_TASK = createActionType('DELETE_TASK');
+const SET_LOADING = createActionType('SET_LOADING');
+
 export const taskReducer = (
   state: TaskReducerStateType = initialState,
-  action: TaskActionType
+  action:
+    | TaskActionType<typeof SET_TASKS, TaskType[]>
+    | TaskActionType<typeof CREATE_TASK, TaskType>
+    | TaskActionType<typeof UPDATE_TASK, TaskType>
+    | TaskActionType<typeof DELETE_TASK, TaskType['_id']>
+    | TaskActionType<typeof SET_LOADING, boolean>
 ): TaskReducerStateType => {
-
   switch (action.type) {
     case SET_TASKS:
       return {
         ...state,
-        tasks: action.payload as TaskType[]
+        tasks: action.payload
       };
 
     case CREATE_TASK:
-      const tasks: TaskType[] = [...state.tasks];
-      const newTask: TaskType = action.payload as TaskType;
-      tasks.forEach((task: TaskType) => task.index = task.status === newTask.status ? task.index + 1 : task.index);
-      tasks.unshift(newTask);
+      const newTask = action.payload;
+      const updatedTasks = state.tasks.map((task) => ({
+        ...task,
+        index: task.status === newTask.status ? task.index + 1 : task.index
+      }));
       return {
         ...state,
-        tasks: tasks
+        tasks: [newTask, ...updatedTasks]
       };
 
     case UPDATE_TASK:
-      const updatedTask: TaskType = action.payload as TaskType;
-      const newAllTasks = [...state.tasks];
-      const updateTaskIndex = state.tasks.findIndex((task: TaskType) =>
-        updatedTask._id && task._id === updatedTask._id ||
-        (task.number === updatedTask.number && task.project === updatedTask.project));
-      newAllTasks.splice(updateTaskIndex, 1);
-      newAllTasks.splice(updateTaskIndex, 0, updatedTask);
+      const updatedTask = action.payload;
+      const updatedTasksList = state.tasks.map((task) =>
+        task._id === updatedTask._id || (task.number === updatedTask.number && task.project === updatedTask.project)
+          ? {...task, ...updatedTask}
+          : task
+      );
       return {
         ...state,
-        tasks: newAllTasks
+        tasks: updatedTasksList
       };
 
     case DELETE_TASK:
-      const taskIdForDelete = action.payload as TaskType['_id'];
-      const tasksWithoutDeleted: TaskType[] = [
-        ...state.tasks.filter((task: TaskType) =>
-          task._id !== taskIdForDelete)
-      ];
+      const taskIdToDelete = action.payload;
+      const tasksWithoutDeleted = state.tasks.filter((task) => task._id !== taskIdToDelete);
       return {
         ...state,
         tasks: tasksWithoutDeleted
@@ -99,7 +73,7 @@ export const taskReducer = (
     case SET_LOADING:
       return {
         ...state,
-        isLoading: action.payload as boolean
+        isLoading: action.payload
       };
 
     default:
@@ -107,18 +81,27 @@ export const taskReducer = (
   }
 };
 
-// action creators
-export const setTasks = (payload: TaskType[]): SET_TASKS_ActionType =>
-  ({ type: SET_TASKS, payload });
+export const setTasks = (payload: TaskType[]): TaskActionType<typeof SET_TASKS, TaskType[]> => ({
+  type: SET_TASKS,
+  payload
+});
 
-export const createTask = (payload: TaskType): CREATE_TASK_ActionType =>
-  ({ type: CREATE_TASK, payload });
+export const createTask = (payload: TaskType): TaskActionType<typeof CREATE_TASK, TaskType> => ({
+  type: CREATE_TASK,
+  payload
+});
 
-export const updateTask = (payload: TaskType): UPDATE_TASK_ActionType =>
-  ({ type: UPDATE_TASK, payload });
+export const updateTask = (payload: TaskType): TaskActionType<typeof UPDATE_TASK, TaskType> => ({
+  type: UPDATE_TASK,
+  payload
+});
 
-export const deleteTask = (payload: TaskType['_id']): DELETE_TASK_ActionType =>
-  ({ type: DELETE_TASK, payload });
+export const deleteTask = (payload: TaskType['_id']): TaskActionType<typeof DELETE_TASK, TaskType['_id']> => ({
+  type: DELETE_TASK,
+  payload
+});
 
-export const setLoadingTasks = (payload: boolean): SET_LOADING_ActionType =>
-  ({ type: SET_LOADING, payload });
+export const setLoadingTasks = (payload: boolean): TaskActionType<typeof SET_LOADING, boolean> => ({
+  type: SET_LOADING,
+  payload
+});
