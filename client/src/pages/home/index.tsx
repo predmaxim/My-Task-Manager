c58512af00d1181e5643edcdf2eac588980cf392
Maxim Predybailo <predmaxim@ya.ru>
2024-12-22 20:58:43 +0000

    fix(client): pages layout

--- client/src/pages/home/index.tsx
+++ client/src/pages/home/index.tsx
@@ -8,11 +8,17 @@ export function HomePage() {
 
   return (
     <div className={`${styles.Home} container`}>
-      <img src="public/img_1.png" alt="image" className={styles.img} />
-      <p className={styles.description}>Stay organized and boost your productivity with our intuitive task manager.
-        Manage your tasks, set priorities,
-        and achieve your goals effortlessly.</p>
-      <Link to={user ? ROUTES.projects : ROUTES.login} className={styles.button}>SignIn</Link>
+      <section className={styles.section}>
+        <div className={styles.hero}>
+          <p className={styles.description}>
+            Stay organized and boost your productivity with our intuitive task manager.
+            Manage your tasks, set priorities,
+            and achieve your goals effortlessly.
+          </p>
+          <Link to={user ? ROUTES.projects : ROUTES.login} className={styles.button}>Start Now</Link>
+        </div>
+        <img src="public/img_1.png" alt="image" className={styles.img} />
+      </section>
     </div>
   );
 }
